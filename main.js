489923c89dfe7fcd869f9a131a9dc59067a328bc
Luciano Cardozo Casariego <lucianoc.c117@gmail.com>
2023-05-31 20:59:43 -0300

    Agregados flags para ingresar una clave a mano, la clave dentro de un archivo, y para descifrar

--- main.js
+++ main.js
@@ -2,35 +2,102 @@
 const CryptoJS = require("crypto-js");
 const fs = require("fs");
 
-const key = "unaClaveDeMinimo32CaracteresParaQueSeaValida";
+key = "";
+type = "encrypt";
 
-{/* TODO: facilitarle al usuario el uso de su propia clave sáb 27 may 2023 19:34:01  */}
-{/* TODO: usando flags cifrar o desifrar el archivo sáb 27 may 2023 19:34:01  */}
-{/* TODO: Completar la ayuda al usuario cuando usa el flag -h sáb 27 may 2023 19:35:20  */}
-{/* TODO: Atrapar el caso en el que se ingrese la ruta "../" sáb 27 may 2023 19:37:42  */}
-{/* TODO: Que el archivo creado tenga el mismo nombre que el original + _cifrado sáb 27 may 2023 19:42:15  */}
+// TODO: Que el archivo creado tenga el mismo nombre que el original + _cifrado o original + _descifrado
+// TODO: Matener el tipo de archivo al crear la encriptacion
+// TODO: Mover logica en otros archivos
+// TODO: Atrapar el caso en el que se ingrese la ruta "../" o similares
+
+//Extras
+//TODO agregar color a las respuetas
+//TODO crear un flag para guardar en un archivo nuevo la key que se haya usado
 
 function main() {
   const argument = process.argv[2];
   if (argument == "-h") {
-    console.log("quiere ver la ayuda");
+    printHelp();
     return;
   }
-  const isValidPAth = verificarRutaExistente(argument);
-  if (!isValidPAth) {
-    console.log("No es una ruta valida");
+
+  const error = setAndValidateData();
+  if (error == 1) {
     return;
   }
+
   const content = readFile(argument);
+  if (type === "encrypt") {
+    const ciphertext = encryptData(content, key);
+    CreateFile("./cifrado.txt", ciphertext);
+  }
 
-  const ciphertext = encryptData(content, key);
-  CreateFile("./cifrado.txt", ciphertext);
-
-  const decryptedData = decryptData(ciphertext, key);
-  console.log("Texto descifrado:", decryptedData);
+  if (type === "decrypt") {
+    const decryptedData = decryptData(content, key);
+    CreateFile("./descifrado.txt", decryptedData);
+  }
 }
 
-function verificarRutaExistente(ruta) {
+function setAndValidateData() {
+  const isValidPAth = checkPath(process.argv[2]);
+  if (!isValidPAth) {
+    console.log("No es una ruta valida");
+    return 1;
+  }
+
+  for (let index = 3; index < process.argv.length; index++) {
+    arg = process.argv[index].split("=");
+    option = arg[0];
+    value = arg[1];
+
+    switch (option) {
+      case "--key":
+        if (key != "") {
+          console.log("tenes que usar o --key o --keyFile, no los dos juntos");
+          return 1;
+        }
+        if (value.length != 32) {
+          console.log("La clave tiene que tener 32 caracteres");
+          return 1;
+        }
+        key = value;
+        break;
+
+      case "--keyFile":
+        if (key != "") {
+          console.log("tenes que usar o --key o --keyFile, no los dos juntos");
+          return 1;
+        }
+        const isValidPAth = checkPath(value);
+        if (!isValidPAth) {
+          console.log("--keyFile no es una ruta valida");
+          return 1;
+        }
+        keyFromfile = readFile(value);
+        if (keyFromfile.length != 32) {
+          console.log("La clave tiene que tener 32 caracteres");
+          return 1;
+        }
+        key = keyFromfile;
+        break;
+
+      case "--decrypt":
+        type = "decrypt";
+        break;
+
+      default:
+        console.log("Algo no salio bien, revisa la ayuda con -h");
+        return 1;
+    }
+  }
+
+  if (key == "") {
+    console.log("Sera usada la clave por defecto");
+    key = "LaclavePorDefectoAlgoritmoAES256";
+  }
+}
+
+function checkPath(ruta) {
   try {
     fs.accessSync(ruta, fs.constants.F_OK);
     return true;
@@ -57,7 +124,16 @@ function CreateFile(filePath, content) {
       return;
     }
 
-    console.log("El archivo se ha creado correctamente.");
+    content = readFile(filePath);
+    if (content == "" && type == "decrypt") {
+      console.log("Fallo");
+      console.log(
+        "Probablemente la clave de descifrado es distinta a la de cifrado"
+      );
+      console.log("  o el archivo ingresado no esta previamente cifrado");
+    } else {
+      console.log("El archivo se ha creado correctamente.");
+    }
   });
 }
 
@@ -80,15 +156,34 @@ function encryptData(data, key) {
 function decryptData(ciphertext, key) {
   // Convertir la clave a formato WordArray
   const keyWordArray = CryptoJS.enc.Utf8.parse(key);
-
   // Realizar el descifrado AES-256 utilizando la clave
   const decrypted = CryptoJS.AES.decrypt(ciphertext, keyWordArray, {
     mode: CryptoJS.mode.ECB, // Modo de operación: Electronic Codebook (ECB)
     padding: CryptoJS.pad.Pkcs7, // Esquema de relleno: PKCS7
   });
-
   // Devolver el texto descifrado como string
   return decrypted.toString(CryptoJS.enc.Utf8);
+}
+
+function printHelp() {
+  console.log("Command: node main.js [FileToEncript] [OPTIONS] \n");
+  console.log("Options:");
+
+  console.log(
+    "-h                     Mostar ayuda en pantalla"
+  );
+  console.log(
+    "--key=[TEXT]           ingresa una clave para encriptar, tiene que ser mayor de 32 caracteres"
+  );
+  console.log(
+    "--keyFile=[PATH]       ingresa el archivo donde este la clave para encriptar, tiene que ser mayor de 32 caracteres"
+  );
+  console.log(
+    "--decrypt              Desencriptar archivo, Por defecto encripta"
+  );
+
+  console.log("\n NOTA: cada opcion se escribe sin espacios, respetando el signo = y el nombre de la opcion ")
+  console.log("las opciones se separan entre si con espacios")
 }
 
 main();
